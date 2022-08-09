5d1a2c5d18dec8a3a547cd4bab5d394746cc801c
“Mason <masonmadaras@gmail.com>
2022-08-09 14:33:22 -0400

    forms react update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [title, setTitle] = useState("this is the title")
+  const [description, setDescription] = useState("your description")
+  const [author,setAuthor] = useState("other")
+
+
+  console.log(title)
+
+
+  function formSubmit(e) {
+    e.preventDefault();
+    console.log("form submitted")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='App'>
+      <form onSubmit={formSubmit}>
+        <h1>Comments</h1>
+
+        {/*  title */}
+        <label>Title</label>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => { setTitle(e.target.value) }}
+          />
+        <h3>{title}</h3>
+
+          {/*  this is a Description */}
+        <label>Description</label>
+        <textarea 
+        value={description}
+        onChange={(e)=>{setDescription(e.target.value) }} />
+        <h3>{description}</h3>
+
+        {/*  Author Name */}
+        <label>Author</label>
+        <select value={author}
+        onChange={(e)=> {setAuthor(e.target.value)}}
         >
-          Learn React
-        </a>
-      </header>
+          <option value="" selected>ChooseOne</option>
+          <option value="UUID">John Doe</option>
+          <option value="#132324">Mason</option>
+          <option value="other">Other</option>
+        </select>
+        <h3>{author}</h3>
+        
+        <button>Submit form</button>
+
+
+      </form>
     </div>
   );
 }
