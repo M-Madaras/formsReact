144dd064366e11d1af12d22d132424bc615c25da
“Mason <masonmadaras@gmail.com>
2022-08-10 21:38:35 -0400

    the correct forms routes advanced react

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,104 +1,119 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from 'react'
 
-export default function Form() {
-    const [formSubmmited, setFormSubmitted] = useState(false)
-    const [validForm, setValidForm] = useState(false)
-    const [errorMessage, setErrorMessage] = useState('')
-    const [form, setForm] = useState({})
-  
-    useEffect(() => {
-      if (form?.title?.length > 3 && form?.description?.length > 10) {
-        setValidForm(true);
-      } else {
-        setValidForm(false);
-      }
-    }, [form])
-  
-  
-    async function formSubmit(e) {
-      e.preventDefault();
-  
-  
-      if (!validForm) {
-        setErrorMessage("Not valid form")
-        return
-      }
-  
-      try {
-        const results = await fetch("https://sql.bocacode.com/comments", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json"
-          },
-          // body: JSON.stringify(comment)
-        })
-        console.log(results)
-        const data = await results.json()
-        console.log(data)
-  
-  
-        setFormSubmitted(true)
-        setErrorMessage("")
-        setValidForm(true)
-        alert("Submitted!!")
-  
-      } catch (error) {
-        console.error(error)
-        setErrorMessage("there was an error submitting you comment" + error.toString())
-      }
-  
+function Form({ setShow, setStateFromChild }) {
+  const [formSubmitted, setFormSubmitted] = useState(false)
+  const [validForm, setValidForm] = useState(false)
+  const [errorMessage, setErrorMessage] = useState('')
+  const [form, setForm] = useState({})
+
+  useEffect(() => {
+    if (
+      form?.title?.length > 5 &&
+      form?.description?.length > 10 &&
+      form.author
+    ) {
+      setValidForm(true)
+    } else {
+      setValidForm(false)
     }
-  
-    console.log('this is form', form)
-  
-    const updateForm = (event) => {
-      setForm({ ...form, [event.target.name]: event.target.value })
+  }, [form])
+
+  // const forSubmit = async (e) => {
+  async function formSubmit(e) {
+    e.preventDefault()
+
+    if (!validForm) {
+      setErrorMessage('Not a valid form')
+      return
+    } else {
+      setErrorMessage('')
     }
-  
-    return (
-      <div className='App'>
-        <form onSubmit={formSubmit}>
-          <h1>Comments</h1>
-          <label>Title</label>
-          <input
-            type='text'
-            name='title'
-            // required
-            value={form.title}
-            onChange={updateForm}
-          />
-          <h3>{form.title}</h3>
-  
-          {/*  this is a Description */}
-          <label>Description</label>
-          <textarea
-            value={form.description}
-            name='description'
-            onChange={updateForm} />
-             <h3>{form.description}</h3>
-  
-  
-          {/*  Author Name */}
-          <label>Author</label>
-          <select value={form.author}
-            onChange={updateForm}
-          > 
-            <option value="" selected>ChooseOne</option>
-            <option value="UUID">John Doe</option>
-            <option value="#132324">Mason</option>
-            <option value="other">Other</option>
-          </select>
-  
-  
-          {!formSubmmited &&
-            <button className="boxbutton">Submit form</button>
-          }
-  
-          {errorMessage &&
-            <h1>There was and Error: <br />
-              {errorMessage}</h1>
-          }
-        </form>
-      </div>
-    );
+    try {
+      //really submit it to an api
+      const results = await fetch('https://sql.bocacode.com/comments', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(form),
+      })
+      console.log(results)
+      const data = await results.json()
+
+      console.log(data)
+
+      setFormSubmitted(true)
+      setShow(false)
+    } catch (error) {
+      console.error(error)
+      setErrorMessage(
+        'There was an error submitting your comment' + error.toString()
+      )
+    }
   }
+
+  console.log('this is form', form)
+
+  // this is best practice for react
+  const updateForm = (event) => {
+    setForm({ ...form, [event.target.name]: event.target.value })
+  }
+
+  return (
+    <div className="App">
+      <form onSubmit={formSubmit}>
+        <h1>Comments</h1>
+
+        {/* Here goes the title */}
+        <label>Title</label>
+        <input
+          type="text"
+          name="title"
+          value={form.title}
+          onChange={updateForm}
+          // onChange={(event) => (console.log(event))}
+        />
+        <h3>{form.title}</h3>
+
+        {/* Here goes the description */}
+        <label>Description</label>
+        <textarea
+          value={form.description}
+          name="description"
+          onChange={updateForm}
+        />
+        <p>
+          <i>{form.description}</i>
+        </p>
+
+        {/* This is the author */}
+        <label>Author</label>
+        <select value={form.author} name="author" onChange={updateForm}>
+          <option value="">Choose One</option>
+          <option value="todd">Todd</option>
+          <option value="mason">Mason</option>
+          <option value="alex">Alex</option>
+          <option value="james">James</option>
+          <option value="">Other</option>
+        </select>
+        <h3>{form.author}</h3>
+
+        <button onClick={() => setStateFromChild('hello father')}>
+          send stuff back to parent
+        </button>
+        <br />
+        <br />
+        {!formSubmitted && <button>Submit</button>}
+        {errorMessage && (
+          <h1>
+            There was an error:
+            <br />
+            {errorMessage}
+          </h1>
+        )}
+      </form>
+    </div>
+  )
+}
+
+export default Form
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,33 @@
-import Form from "../components/Form";
+import Form from '../components/Form'
+import { Button, Modal } from 'react-bootstrap'
+import { useState } from 'react'
 
-const Contact = () => (
+const Contact = () => {
+  const [show, setShow] = useState(false)
+  const [stateFromChild, setStateFromChild] = useState('')
+
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
+
+    console.log('state from child', stateFromChild)
+
+  return (
     <>
-        <h3>this is contact</h3>
-        <Form />
+      <h3>This is contact {stateFromChild} </h3>
+      <Button variant="primary" onClick={handleShow}>
+        Contact us
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Contact form</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form setShow={setShow} setStateFromChild={setStateFromChild} />
+        </Modal.Body>
+      </Modal>
     </>
-)
-export default Contact;
+  )
+}
+
+export default Contact
